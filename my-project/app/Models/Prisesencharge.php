94d442e7236dede94eb3d3b70677386428a5cb5f
othmaneuw <strangeothmane@gmail.com>
2023-08-16 11:55:50 +0100

    backup sql

--- my-project/app/Models/Prisesencharge.php
+++ my-project/app/Models/Prisesencharge.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\Auth;
 
 class Prisesencharge extends Model
 {
+
+   
     
 }
