04fd08ed0cf31f41622a70d3902e53e37693a50d
othmaneuw <strangeothmane@gmail.com>
2023-08-22 11:12:11 +0100

    widgets

---
+++ my-project/app/Widgets/PriseEnCharges.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Widgets;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Widgets\BaseDimmer;
+
+class PriseEnCharges extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Models\Prisesencharge::count();
+        $string = "Prises En Charges";
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-treasure',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.post_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => 'Prises En Charge Médicales',
+                'link' => route('voyager.prisesencharges.index'),
+            ],
+            'image' => '/prisesencharge-bg.jpg',
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('Post'));
+    }
+}
