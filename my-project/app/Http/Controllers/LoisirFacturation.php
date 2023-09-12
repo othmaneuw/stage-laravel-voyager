0ddb82e3f9775ceb24d2ac87455aa02271afe4bf
othmaneuw <strangeothmane@gmail.com>
2023-09-12 13:51:21 +0100

    editer facturation

--- my-project/app/Http/Controllers/LoisirFacturation.php
+++ my-project/app/Http/Controllers/LoisirFacturation.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ModifierFacturation;
 use App\Mail\NouvelleFacturation;
 use App\Models\Assurancefacturation;
 use App\Models\Facturation;
@@ -290,4 +291,84 @@ class LoisirFacturation extends VoyagerBaseController
         $email = DB::table('users')->where('id',$user)->get()->first()->email;
         Mail::to($email)->send(new NouvelleFacturation());
     }
+
+
+    private function updateRemboursement($data){
+        $facturation = Loisirsfacturation::where('numerofacture',$data->get('numerofacture'))->get()->first();
+        $remboursement = Loisirsremboursement::where('facture',$facturation->id)->get()->first();
+        $remboursement->montant = $data->get('montant');
+        $remboursement->demandeur = $data->get('user');
+        $remboursement->activite = $data->get('activite');
+        $remboursement->facture = $facturation->id;
+        $remboursement->periodicite = "one";
+        $remboursement->save();
+        $user = DB::table('users')->where('id',$data->get('user'))->get()->first();
+        $user_name = $user->name;
+        $user_email = $user->email;
+        Mail::to($user_email)->send(new ModifierFacturation($user_name));
+        // echo "<pre>";
+        // var_dump($data);
+        // die();
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Compatibility with Model binding.
+        $id = $id instanceof \Illuminate\Database\Eloquent\Model ? $id->{$id->getKeyName()} : $id;
+
+        $model = app($dataType->model_name);
+        $query = $model->query();
+        if ($dataType->scope && $dataType->scope != '' && method_exists($model, 'scope'.ucfirst($dataType->scope))) {
+            $query = $query->{$dataType->scope}();
+        }
+        if ($model && in_array(SoftDeletes::class, class_uses_recursive($model))) {
+            $query = $query->withTrashed();
+        }
+
+        $data = $query->findOrFail($id);
+
+        // Check permission
+        $this->authorize('edit', $data);
+
+        // Validate fields with ajax
+        $val = $this->validateBread($request->all(), $dataType->editRows, $dataType->name, $id)->validate();
+
+        // Get fields with images to remove before updating and make a copy of $data
+        $to_remove = $dataType->editRows->where('type', 'image')
+            ->filter(function ($item, $key) use ($request) {
+                return $request->hasFile($item->field);
+            });
+        $original_data = clone($data);
+
+        $this->insertUpdateData($request, $slug, $dataType->editRows, $data);
+
+        $this->updateRemboursement($request->request);
+
+
+
+
+        // Delete Images
+        $this->deleteBreadImages($original_data, $to_remove);
+
+        event(new BreadDataUpdated($dataType, $data));
+
+
+        if (auth()->user()->can('browse', app($dataType->model_name))) {
+            $redirect = redirect()->route("voyager.{$dataType->slug}.index");
+        } else {
+            $redirect = redirect()->back();
+        }
+
+        return $redirect->with([
+            'message'    => __('voyager::generic.successfully_updated')." {$dataType->getTranslatedAttribute('display_name_singular')}",
+            'alert-type' => 'success',
+        ]);
+    }
+
+
 }
